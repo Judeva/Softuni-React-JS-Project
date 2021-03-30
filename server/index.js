ce3f07aba1c69c1b6035589c4fd1c834a56a4c6b
Joanna Doudeva <joanna.doudeva@gmail.com>
2021-03-30 21:06:19 +0300

    Server settings

--- server/index.js
+++ server/index.js
@@ -6,11 +6,13 @@ const routes = require('./routes');
 const app = express();
 
 app.use(cors());
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 app.get('/', (req, res)=>{
     res.json({message:'it is working'});
 });
 
-app.use(routes);
+app.use('/api', routes);
 
 app.listen(5000, console.log.bind(console, 'Server is listening on port 5000...'));
