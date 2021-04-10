3b2e2b2562e340078ddc05b855810b5e91b33ca0
Joanna Doudeva <joanna.doudeva@gmail.com>
2021-04-10 13:47:08 +0300

    Css home pictures grid

--- client/src/components/nomination/Nomination.js
+++ client/src/components/nomination/Nomination.js
@@ -6,17 +6,15 @@ const Nomination = ({
     doc
 }) => {
 
-    console.log(doc);
     return (
-        <div className="card">
+        <div className="img-wrap">
             <img src={doc.imageUrl} className="card-img-top" alt="photo"></img>
-            <div className="card-body">
-                <h5 className="card-title">{doc.title}</h5>
-                <p className="card-text">{doc.description}</p>
-            </div>
+            <h5 className="card-title">{doc.title}</h5>
+            <p className="card-text">{doc.description}</p>
+            
             <Link to="/" className="btn btn-primary">Don't press</Link>
         </div>
-    )
+    );
 
 }
 
